a8ca6f2fa81bebc534fd05aec2228560b1bc352d
ChoiGunWoo1 <gunwoo0813@naver.com>
2025-05-09 11:13:46 +0900

    [Gold V] Title: 괄호의 값, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Gold/2504. 괄호의 값/괄호의 값.cc
+++ 백준/Gold/2504. 괄호의 값/괄호의 값.cc
@@ -1,78 +1,68 @@
 #include <iostream>
-#include <string>
+#include <algorithm>
 #include <stack>
 
 using namespace std;
 
-stack<char> row;
-stack<int> value;
+
+stack<char> comp;
 
 
-int solution(string input) {
-    
-    int answer = 0;
-
-    int tmp = 1;
-
-    for(int i = 0; i < input.length(); i++) {
-        char now = input[i];
-        if(input[i] == '(') {
-            tmp = tmp * 2;
-            value.push(now);
-        }
-        else if(input[i] == '[') {
-            tmp = tmp * 3;
-            value.push(now);
-        }
-        else if(input[i] == ')') {
-            if(value.empty() || value.top() != '(') {
-                answer = 0;
-                break;
-            }
-            if(input[i-1] == '(') {
-                answer += tmp;
-                tmp = tmp / 2;
-                value.pop();
-            }
-            else {
-                tmp = tmp / 2;
-                value.pop();
-
-            }
-        }
-        else if(input[i] == ']') {
-            if(value.empty() || value.top() != '[') {
-                answer = 0;
-                break;
-            }
-            if(input[i-1] == '[') {
-                answer += tmp;
-                tmp = tmp / 3;
-                value.pop();
-            }
-            else {
-                tmp = tmp / 3;
-                value.pop();
-            }
-        }
-    }
-    if(!value.empty()) {
-        answer = 0;
-    }
-    return answer;
-
-}
-
-int main(void) {
+int main(void)
+{
     ios::sync_with_stdio(0);
     cin.tie(0);
+    string s;
+    cin >> s;
+    int answer = 0;
+    int temp = 1;
+    for(int i = 0; i < s.length(); i++) {
+        char now = s[i];
+        if(now == '(') {
+            temp *= 2;
+            comp.push(now);
+        }
+        else if(now == '[') {
+            temp *= 3;
+            comp.push(now);
+        }
+        else if(now == ')') {
+            if(comp.empty() || comp.top() != '(') {
+                answer = 0;
+                break;
+            }
+            if(s[i-1] == '(') {
+                answer += temp;
+                temp /= 2;
+            }
+            else {
+                temp /=2;
+            }
+            comp.pop();
+        }
+        else if(now == ']') {
+            if(comp.empty() || comp.top() != '[') {
+                answer = 0;
+                break;
+            }
+            if(s[i-1] == '[') {
+                answer += temp;
+                temp /= 3;
+            }
+            else {
+                temp /= 3;
+            }
+            comp.pop();
+        }
+        
+    }
+    if(!comp.empty()) {
+        answer = 0;
+    }
     
-    string i;
-    cin >> i;
-    cout << solution(i);
+    cout << answer;
     
 
-    
 
     return 0;
 }
